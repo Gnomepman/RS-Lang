ed577907b1c4c94840f0e31bc7907734fc8e8c67
Danya <danilomaster2003@ukr.net>
2022-08-24 09:13:53 +0300

    feat: add files

---
+++ src/pages/sprint_game/sprint_game.ts
@@ -0,0 +1,12 @@
+import './sprint_game.scss';
+import Page from '../../components/templates/page';
+
+export default class Sprint_game extends Page {
+    constructor(id: string) {
+        super(id);
+    }
+
+    render() {
+        return this.container
+    }
+}
