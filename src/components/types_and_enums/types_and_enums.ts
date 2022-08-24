6af5ebaed36b183f4fe7fdf8a870b679c8244eb2
Danya <danilomaster2003@ukr.net>
2022-08-24 15:39:52 +0300

    feat: add ids for two mini-games

--- src/components/types_and_enums/types_and_enums.ts
+++ src/components/types_and_enums/types_and_enums.ts
@@ -3,7 +3,9 @@ export const enum PageIds {
     MainPage = 'main-page',
     LearningPage = 'learning-page',
     MiniGamesPage = 'mini-games-page',
-    StatisticsPage = 'statistics-page'
+    StatisticsPage = 'statistics-page',
+    SprintGame = 'sprint',
+    AudioGame = 'audio-challenge' 
 }
 
 export const enum ErrorTypes {
